5c846ca761848e4d8ed59680af76d7779f4aa59f
luckyjoke <1335131780@qq.com>
2021-04-23 20:02:56 +0800

    初始化全局样式表

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
 
 <style lang="less">
+  @import "assets/css/base.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
