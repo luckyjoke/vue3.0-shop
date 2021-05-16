76910a56e3d786f587fb25fcf588d8e88eb6344f
luckyjoke <1335131780@qq.com>
2021-05-16 18:13:19 +0800

    更新关于作者信息

--- src/views/user/AboutUs/AboutUs.vue
+++ src/views/user/AboutUs/AboutUs.vue
@@ -8,12 +8,14 @@
 			<div class="animate__animated animate__backInUp">
 				<h3 style="margin-top: 20px;">大三学生喜欢钻研技术，爱好运动剪辑</h3>
 				<h3 style="margin-top: 20px;">梦想是环游世界🌏</h3>
+				<h3 style="margin-top: 20px;">🈲项目仅供学习与交流</h3>
 			</div>
 			<div class="social-info animate__animated animate__backInUp">
 				<span><a href="https://github.com/luckyjoke">Github |</a></span>
 				<span><a href="https://gitee.com/luckyjoke"> Gitee  |</a></span>
 				<span><a href="https://luckyjoke.gitee.io/"> 个人网站  |</a></span>
-				<span><a href="https://juejin.cn/user/3966693685604951"> 掘金</a></span>			
+				<span><a href="https://juejin.cn/user/3966693685604951">  掘金  |</a></span>	
+				<span>  📧zhangweiquan7@foxmail.com</span>						
 			</div>
 
 		</div>
